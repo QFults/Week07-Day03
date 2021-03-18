55d4226c7160051d5225750d88e62b63f1baa3b8
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-18 11:13:13 -0700

    code

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const { User } = require('../models')
+
+router.post('/users/register', (req, res) => {
+  const { name, email, username } = req.body
+  User.register(new User({ name, email, username }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.post('/users/login', (req, res) => {
+  User.authenticate()(req.body.username, req.body.password, (err, user) => {
+    if (err) { console.log(err) }
+    res.json(user)
+  })
+})
+
+module.exports = router
